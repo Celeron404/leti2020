5bbac42e89d307e6c2e07aeeb1f7f2aee59d68c8
Zimatskiy-S <46576449+Zimatskiy-S@users.noreply.github.com>
2020-10-18 13:12:45 +0300

    Решены первые два задания

--- Practical_work_1/Practical_work_1.cpp
+++ Practical_work_1/Practical_work_1.cpp
@@ -3,62 +3,81 @@
 
 using namespace std;
 
-void binaryPresentation(int);
+void toBinary(int);
 
 int main()
 {
 	setlocale(0, "");
-	cout << "Данная программа является решением первой практической работы" << endl
-		<< "\"Типы данных и их внутреннее представление в памяти\"" << endl << endl;
+	cout << "Данная программа является решением первой практической работы \n"
+		<< "\"Типы данных и их внутреннее представление в памяти\" \n \n";
 
-	cout << "Задание 1: размер данных на компьютере" << endl
-		<< "int: \t\t" << sizeof(int) << " байта." << endl
-		<< "short int \t" << sizeof(short int) << " байта." << endl
-		<< "long int \t" << sizeof(long int) << " байта." << endl
-		<< "float \t\t" << sizeof(float) << " байта." << endl
-		<< "double \t\t" << sizeof(double) << " байтов." << endl
-		<< "long double \t" << sizeof(long double) << " байтов." << endl
-		<< "char \t\t" << sizeof(char) << " байт." << endl
-		<< "bool \t\t" << sizeof(bool) << " байт." << endl << endl;
+	cout << "\tЗадание 1: размер данных на компьютере \n"//												Решение первого задания
+		<< "int: \t\t" << sizeof(int) << " байта. \n"
+		<< "short int \t" << sizeof(short int) << " байта. \n"
+		<< "long int \t" << sizeof(long int) << " байта. \n"
+		<< "float \t\t" << sizeof(float) << " байта. \n"
+		<< "double \t\t" << sizeof(double) << " байтов. \n"
+		<< "long double \t" << sizeof(long double) << " байтов. \n"
+		<< "char \t\t" << sizeof(char) << " байт. \n"
+		<< "bool \t\t" << sizeof(bool) << " байт. \n \n";
 
-	cout << "Задание 2:" << endl
-		<< "Введите любое целое число диапазона int..." << endl;
-	int input;
-	cin >> input;
-	cout << "Двоичное представление в памяти числа " << input << ": ";
-	binaryPresentation(input);
-	cout << endl;
+	cout << "\tЗадание 2: двоичное представление в памяти (все разряды) числа типа int \n";//			Решение второго задания
+	while (true) {
+		cout << "Введите любое целое число диапазона int... \n";
+		double input;
+		cin >> input;
+
+
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Ошибка! Введены некорректные данные. \n";
+			continue;
+		} else
+		if (static_cast<int>(input) != input) { // Проверка на целое число
+			cout << "Необходимо ввести целое число! \n";
+			continue;
+		}
+		
+		cout << "Двоичное представление в памяти числа " << input << ": ";
+		toBinary(input);
+
+		cout << "\nДля повтора введите 1, иначе произойдёт переход к следующему заданию... \n";
+		int answer;
+		cin >> answer;
+		if (answer != 1)
+			break;
+	}
 
 	system("pause");
 	return 0;
 }
 
-void binaryPresentation(int value) {/*
+void toBinary(int value) {
+	int mask = 1 << 31; // Маска представляет собой 2^32, то есть 10...00, где нулей 31.
 	for (int i = 1; i <= 32; i++) {
-		putchar(value & 1 ? '1' : '0');
-		value >>= 1;
-		if (i % 8 == 0)
-			putchar(' ');
-		if (i % 31 == 0)
-			putchar(' ');*/
-
-	unsigned int order = 32; // Количество разрядов
-	unsigned int mask = 1 << order - 1; // Маска побитового сравнения
-	cout << endl << "order = " << order << endl
-		<< "mask = " << mask << endl;
-	for (int i = 1; i <= order; i++)
-	{
+		/*
+		Побитовое сравнение введённого числа с маской.
+		Если у числа и маски есть хотя бы одна общая единица (в данном случае бОльший бит числа), то выводится "1", иначе "0".
+		Далее побитовый сдвиг введённого числа влево и так 32 раза (именно столько бит определяется под int).
+		Например:
+		0 1000000 00000000 00000000 00000000 - двоичное представление в памяти компьютера введённого числа 2^31
+		1 0000000 00000000 00000000 00000000 - маска (2^32)
+		Вывод: 0
+		Затем побитовый сдвиг введённого числа влево:
+		1 0000000 00000000 00000000 00000000
+		1 0000000 00000000 00000000 00000000 - маска (2^32)
+		Вывод: 1
+		Далее следуют нули, в выводе: 0 1000000 00000000 00000000 00000000
+		*/
 		putchar(value & mask ? '1' : '0');
-		value <<= 1; // Побитовый сдвиг числа
+		value <<= 1;
 		if (i % 8 == 0)
-		{
-			putchar(' ');
-		}
-		if (i % order - 1 == 0)
-		{
-			putchar(' ');
-		}
+			putchar(' '); // отделение друг от друга байтов, содержащих значение числа
+		if (i == 1)
+			putchar(' '); // отделение знакового бита
 	}
-	cout << endl << "order = " << order << endl
-		<< "mask = " << mask << endl;
 }
+
+//void toBinary()
